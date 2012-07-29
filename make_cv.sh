3b0dd48219453e116dc8a079a774cae3ba4c6da8
Anthony Scopatz <scopatz@gmail.com>
2012-07-29 16:17:48 -0500

    More minor changes.

--- make_cv.sh
+++ make_cv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-latex scopatz_cv.tex
+latex scopatz_cv.tex -halt-on-error
 bibtex scopatz_cv.aux
-latex scopatz_cv.tex
-latex scopatz_cv.tex
+latex scopatz_cv.tex -halt-on-error
+latex scopatz_cv.tex -halt-on-error
 dvipdf scopatz_cv.dvi
